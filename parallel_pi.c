319f74b61b97dad0902fbd25716753462824ae1c
Elouan1411 <boiteux.elouan@gmail.com>
2024-12-12 17:27:03 +0100

    fix bug

--- parallel_pi.c
+++ parallel_pi.c
@@ -2,16 +2,14 @@
 #include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
 #include <time.h>
 #include <unistd.h>
 #include <wait.h>
-#include <sys/types.h>
 
 volatile int stop = 0;
 
-double get_double_0_1() {
-    return (double)random() / INT_MAX;
-}
+double get_double_0_1() { return (double)random() / INT_MAX; }
 
 int compute_pi() {
     double x = get_double_0_1();
@@ -43,32 +41,6 @@ void install_signal_handler() {
         exit(1);
     }
 }
-
-void create_pipes_and_forks(int NPROC, int*** tube_p, int*** tube_n, pid_t** pid) {
-    *tube_p = (int**)malloc(NPROC * sizeof(int*));
-    *tube_n = (int**)malloc(NPROC * sizeof(int*));
-    *pid = (pid_t*)malloc(NPROC * sizeof(pid_t));
-
-    for (int i = 0; i < NPROC; i++) {
-        (*tube_p)[i] = (int*)malloc(2 * sizeof(int));
-        (*tube_n)[i] = (int*)malloc(2 * sizeof(int));
-        if (pipe((*tube_p)[i]) == -1) {
-            perror("pipe_p");
-            exit(1);
-        }
-        if (pipe((*tube_n)[i]) == -1) {
-            perror("pipe_n");
-            exit(1);
-        }
-        (*pid)[i] = fork();
-        if ((*pid)[i] == -1) {
-            perror("fork");
-            exit(1);
-        }
-    }
-}
-
-
 void process_child(int i, int** tube_p, int** tube_n) {
     myClose(tube_n[i][0], "close_n read son");
     myClose(tube_p[i][0], "close_p read son");
@@ -91,8 +63,36 @@ void process_child(int i, int** tube_p, int** tube_n) {
     myClose(tube_n[i][1], "close_n write son");
     exit(0);
 }
+void create_pipes_and_forks(int NPROC, int*** tube_p, int*** tube_n,
+                            pid_t** pid) {
+    *tube_p = (int**)malloc(NPROC * sizeof(int*));
+    *tube_n = (int**)malloc(NPROC * sizeof(int*));
+    *pid = (pid_t*)malloc(NPROC * sizeof(pid_t));
 
-void read_and_close_pipes(int i, int** tube_p, int** tube_n, unsigned long* p_total, unsigned long* n_total) {
+    for (int i = 0; i < NPROC; i++) {
+        (*tube_p)[i] = (int*)malloc(2 * sizeof(int));
+        (*tube_n)[i] = (int*)malloc(2 * sizeof(int));
+        if (pipe((*tube_p)[i]) == -1) {
+            perror("pipe_p");
+            exit(1);
+        }
+        if (pipe((*tube_n)[i]) == -1) {
+            perror("pipe_n");
+            exit(1);
+        }
+        (*pid)[i] = fork();
+        if ((*pid)[i] == -1) {
+            perror("fork");
+            exit(1);
+        }
+        if ((*pid)[i] == 0) {
+            process_child(i, *tube_p, *tube_n);
+        }
+    }
+}
+
+void read_and_close_pipes(int i, int** tube_p, int** tube_n,
+                          unsigned long* p_total, unsigned long* n_total) {
     unsigned long p_temp = 0;
     unsigned long n_temp = 0;
 
@@ -149,14 +149,16 @@ int main(int argc, char* argv[]) {
     install_signal_handler();
     create_pipes_and_forks(NPROC, &tube_p, &tube_n, &pid);
 
-    for (int i = 0; i < NPROC; i++) {
-        if (pid[i] == 0) {
-            process_child(i, tube_p, tube_n);
-        }
+    int n = 0;
+    int p = 0;
+    while (!stop) {
+        p += compute_pi();
+        n++;
     }
 
-    unsigned long n_total = 0;
-    unsigned long p_total = 0;
+    unsigned long n_total = n;
+    unsigned long p_total = p;
+
     for (int i = 0; i < NPROC; i++) {
         read_and_close_pipes(i, tube_p, tube_n, &p_total, &n_total);
         free(tube_p[i]);
